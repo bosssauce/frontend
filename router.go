acc674037adceeb5813646f852e475d7d0aec058
Steve Manuel <nilslice@gmail.com>
2017-08-01 02:46:04 -0600

    embedding mux.Router so new exported funcs can be added

--- router.go
+++ router.go
@@ -6,14 +6,28 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type router struct {
+	mux *mux.Router
+}
+
 var (
-	// export the Router so any content type file can just call frontend.Router.HandleFunc(....
+	// Router is exported so any content type file can just call frontend.Router.HandleFunc(....
 	// otherwise, a New() func might be needed, but it is not clear where it should be called from
-	Router *mux.Router
+	Router *router
 )
 
+func (r *router) HandleFunc(path string, fn func(http.ResponseWriter, *http.Request)) {
+	r.mux.HandleFunc(path, fn)
+}
+
+func (r *router) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	r.mux.ServeHTTP(res, req)
+}
+
 func init() {
-	Router = mux.NewRouter()
+	Router = &router{
+		mux: mux.NewRouter(),
+	}
 
 	http.Handle("/", Router)
 }
